442db145d1ea7220f9d64ef3abe6f6c5bacaeb99
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-06-13 12:56:21 +0600

    Pull in Linux

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.filechooser import FileChooserListView
 
-
 class DashboardScreen(Screen):
     pass
-
 
 class DataUploadScreen(Screen):
     def open_file_selector(self):
@@ -14,19 +12,16 @@ class DataUploadScreen(Screen):
         filechooser = FileChooserListView()
         self.add_widget(filechooser)
 
-
 class DataProcessingScreen(Screen):
     pass
-
 
 class MainApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(DashboardScreen(name="dashboard"))
-        sm.add_widget(DataUploadScreen(name="data_upload"))
-        sm.add_widget(DataProcessingScreen(name="data_processing"))
+        sm.add_widget(DashboardScreen(name='dashboard'))
+        sm.add_widget(DataUploadScreen(name='data_upload'))
+        sm.add_widget(DataProcessingScreen(name='data_processing'))
         return sm
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     MainApp().run()
